8a84738427b2d95f07bbeb9872bb69dd360b30d7
William Bae <williambae1@gmail.com>
2017-06-01 23:07:34 +0900

    revert pattern

--- client/modules/alarm.py
+++ client/modules/alarm.py
@@ -67,4 +67,4 @@ def handle(text, mic, profile):
 
 
 def isValid(text):
-    return bool(re.search(ur'\b알람을? 추가해?\b', text, re.IGNORECASE | re.UNICODE))
+    return bool(re.search(ur'\b알람을 추가\b', text, re.IGNORECASE | re.UNICODE))
