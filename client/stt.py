09d6487a25b741cdf5c4d3509e4fde0b939764a2
William Bae <williambae1@gmail.com>
2017-06-01 21:37:37 +0900

    Added Google Cloud STT

--- client/stt.py
+++ client/stt.py
@@ -8,6 +8,7 @@ import logging
 import urllib
 import urlparse
 import re
+import base64
 import subprocess
 from abc import ABCMeta, abstractmethod
 import requests
@@ -410,6 +411,164 @@ class GoogleSTT(AbstractSTTEngine):
                 raise ValueError('Nothing has been transcribed.')
             results = [alt['transcript'] for alt
                        in response['result'][0]['alternative']]
+        except ValueError as e:
+            self._logger.warning('Empty response: %s', e.args[0])
+            results = []
+        except (KeyError, IndexError):
+            self._logger.warning('Cannot parse response.', exc_info=True)
+            results = []
+        else:
+            # Convert all results to uppercase
+            results = tuple(result.upper() for result in results)
+            self._logger.info('Transcribed: %r', results)
+        return results
+
+    @classmethod
+    def is_available(cls):
+        return diagnose.check_network_connection()
+
+
+class GoogleCloudSTT(AbstractSTTEngine):
+    """
+    Speech-To-Text implementation which relies on the Google Cloud Speech API.
+
+    This implementation requires a Google API key to be present in profile.yml
+
+    Excerpt from sample profile.yml:
+
+        ...
+        timezone: US/Pacific
+        stt_engine: google-cloud
+        keys:
+            GOOGLE_CLOUD_SPEECH: $YOUR_KEY_HERE
+
+    """
+
+    SLUG = 'google-cloud'
+
+    def __init__(self, api_key=None, language='ko-KR'):
+        # FIXME: get init args from config
+        """
+        Arguments:
+        api_key - the public api key which allows access to Google APIs
+        """
+        self._logger = logging.getLogger(__name__)
+        self._request_url = None
+        self._language = None
+        self._api_key = None
+        self._http = requests.Session()
+        self.language = language
+        self.api_key = api_key
+
+    @property
+    def request_url(self):
+        return self._request_url
+
+    @property
+    def language(self):
+        return self._language
+
+    @language.setter
+    def language(self, value):
+        self._language = value
+        self._regenerate_request_url()
+
+    @property
+    def api_key(self):
+        return self._api_key
+
+    @api_key.setter
+    def api_key(self, value):
+        self._api_key = value
+        self._regenerate_request_url()
+
+    def _regenerate_request_url(self):
+        if self.api_key and self.language:
+            query = urllib.urlencode({'key': self.api_key})
+            self._request_url = urlparse.urlunparse(
+                ('https', 'speech.googleapis.com', '/v1/speech:recognize', '',
+                 query, ''))
+        else:
+            self._request_url = None
+
+    @classmethod
+    def get_config(cls):
+        # FIXME: Replace this as soon as we have a config module
+        config = {}
+        # HMM dir
+        # Try to get hmm_dir from config
+        profile_path = jasperpath.config('profile.yml')
+        if os.path.exists(profile_path):
+            with open(profile_path, 'r') as f:
+                profile = yaml.safe_load(f)
+                if 'keys' in profile and 'GOOGLE_CLOUD_SPEECH' in profile['keys']:
+                    config['api_key'] = profile['keys']['GOOGLE_CLOUD_SPEECH']
+        return config
+
+    def transcribe(self, fp):
+        """
+        Performs STT via the Google Speech API, transcribing an audio file and
+        returning an English string.
+
+        Arguments:
+        audio_file_path -- the path to the .wav file to be transcribed
+        """
+
+        if not self.api_key:
+            self._logger.critical('API key missing, transcription request ' +
+                                  'aborted.')
+            return []
+        elif not self.language:
+            self._logger.critical('Language info missing, transcription ' +
+                                  'request aborted.')
+            return []
+
+        wav = wave.open(fp, 'rb')
+        frame_rate = wav.getframerate()
+        wav.close()
+        data = base64.b64encode(fp.read())
+
+        content = json.dumps({
+        	'config': {
+        		'encoding': "LINEAR16",
+        		'sampleRateHertz': frame_rate,
+        		'languageCode': self.language,
+        		'maxAlternatives': 1,
+        		'speechContexts': [
+        			{
+        				'phrases': [ u'누리' ]  # TODO
+        			}
+        		]
+        	},
+        	'audio': {
+        		'content': data
+        	}
+    	})
+        headers = {'content-type': 'text/json'}
+        r = self._http.post(self.request_url, data=content, headers=headers)
+        try:
+            r.raise_for_status()
+        except requests.exceptions.HTTPError:
+            self._logger.critical('Request failed with http status %d',
+                                  r.status_code)
+            self._logger.critical(r.text)
+            if r.status_code == requests.codes['forbidden']:
+                self._logger.warning('Status 403 is probably caused by an ' +
+                                     'invalid Google API key.')
+            return []
+        r.encoding = 'utf-8'
+        try:
+            # We cannot simply use r.json() because Google sends invalid json
+            # (i.e. multiple json objects, seperated by newlines. We only want
+            # the last one).
+            #response = json.loads(list(r.text.strip().split('\n', 1))[-1])
+            response = r.json()
+            print(r.text)
+            if not response or len(response['results']) == 0:
+                # Response result is empty
+                raise ValueError('Nothing has been transcribed.')
+            results = [alt['transcript'] for alt
+                       in response['results'][0]['alternatives']]
         except ValueError as e:
             self._logger.warning('Empty response: %s', e.args[0])
             results = []
